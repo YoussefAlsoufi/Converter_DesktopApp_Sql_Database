18f8873f3a3dbfb6c1d3863654ce1e27e1e84782
Youssef Alsoufi <60790715+YoussefAlsoufi@users.noreply.github.com>
2023-07-24 12:45:20 +0200

    Update MyConnection.cs
    Enhance the Connection class

--- MyConnection.cs
+++ MyConnection.cs
@@ -1,61 +1,109 @@
-﻿using System.Collections.Generic;
 using System.Data.SqlClient;
 
 namespace Converter_DesktopApp_Sql_Database
 {
-
     public class MyConnection
     {
-        private readonly SqlConnection connection = GetConnection();
-        public static SqlConnection GetConnection()
+        private readonly string connectionString;
+
+        public MyConnection(string connectionString)
         {
-            return new SqlConnection() { ConnectionString = "server=DESKTOP-DTPF1JL; database=Converter; Integrated Security=true " }; 
+            this.connectionString = connectionString;
         }
 
         public void InsertCategory(int newCateId, string cateName)
         {
-            connection.Open();
-            SqlCommand insertCate = new($"insert into dbo.CATEGORIES (CATE_ID,CATE_NAME) values ({newCateId + 1},'{cateName}')", connection);
-            _ = insertCate.ExecuteNonQuery();
-            connection.Close();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
 
+                // Use parameterized query to prevent SQL injection
+                string query = "INSERT INTO dbo.CATEGORIES (CATE_ID, CATE_NAME) VALUES (@CateId, @CateName)";
+                using (var insertCate = new SqlCommand(query, connection))
+                {
+                    insertCate.Parameters.AddWithValue("@CateId", newCateId + 1);
+                    insertCate.Parameters.AddWithValue("@CateName", cateName);
+
+                    _ = insertCate.ExecuteNonQuery();
+                }
+            }
         }
+
         public void InsertUnit(int newUnitId, string unitName, int newCateId, string value)
         {
-            connection.Open();
-            SqlCommand insertUnit = new($"insert into dbo.UNITS (UNIT_ID,UNIT_NAME,CATE_ID,VALUE) values ({newUnitId + 1},'{unitName}',{newCateId},'{value}')", connection);
-            _ = insertUnit.ExecuteNonQuery();
-            connection.Close();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
 
+                // Use parameterized query to prevent SQL injection
+                string query = "INSERT INTO dbo.UNITS (UNIT_ID, UNIT_NAME, CATE_ID, VALUE) VALUES (@UnitId, @UnitName, @CateId, @Value)";
+                using (var insertUnit = new SqlCommand(query, connection))
+                {
+                    insertUnit.Parameters.AddWithValue("@UnitId", newUnitId + 1);
+                    insertUnit.Parameters.AddWithValue("@UnitName", unitName);
+                    insertUnit.Parameters.AddWithValue("@CateId", newCateId);
+                    insertUnit.Parameters.AddWithValue("@Value", value);
+
+                    _ = insertUnit.ExecuteNonQuery();
+                }
+            }
         }
+
         public void DeleteCategory(string cateName, int cateId)
         {
-            //delete the Cate:
-            connection.Open();
-            SqlCommand deleteCate = new($"delete from dbo.CATEGORIES where CATE_NAME = '{cateName}';", connection);
-            _ = deleteCate.ExecuteNonQuery();
-            connection.Close();
-            //delete all related Units:
-            connection.Open();
-            SqlCommand deleteUnit = new($"delete from dbo.UNITS where CATE_ID = {cateId};", connection);
-            _ = deleteUnit.ExecuteNonQuery();
-            connection.Close();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
 
+                // Use parameterized query to prevent SQL injection
+                string deleteCateQuery = "DELETE FROM dbo.CATEGORIES WHERE CATE_NAME = @CateName";
+                using (var deleteCate = new SqlCommand(deleteCateQuery, connection))
+                {
+                    deleteCate.Parameters.AddWithValue("@CateName", cateName);
+                    _ = deleteCate.ExecuteNonQuery();
+                }
+
+                //delete all related Units:
+                string deleteUnitQuery = "DELETE FROM dbo.UNITS WHERE CATE_ID = @CateId";
+                using (var deleteUnit = new SqlCommand(deleteUnitQuery, connection))
+                {
+                    deleteUnit.Parameters.AddWithValue("@CateId", cateId);
+                    _ = deleteUnit.ExecuteNonQuery();
+                }
+            }
         }
+
         public void DeleteUnit(string unitName)
         {
-            connection.Open();
-            SqlCommand deleteUnit = new($"delete from dbo.UNITS where UNIT_NAME = '{unitName}';", connection);
-            _ = deleteUnit.ExecuteNonQuery();
-            connection.Close();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Use parameterized query to prevent SQL injection
+                string deleteUnitQuery = "DELETE FROM dbo.UNITS WHERE UNIT_NAME = @UnitName";
+                using (var deleteUnit = new SqlCommand(deleteUnitQuery, connection))
+                {
+                    deleteUnit.Parameters.AddWithValue("@UnitName", unitName);
+                    _ = deleteUnit.ExecuteNonQuery();
+                }
+            }
         }
+
         public void EditValue(string unitName, string value)
         {
-            connection.Open();
-            SqlCommand editValue = new($"UPDATE dbo.UNITS SET VALUE = {value} WHERE UNIT_NAME = '{unitName}' ", connection);
-            _ = editValue.ExecuteNonQuery();
-            connection.Close();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Use parameterized query to prevent SQL injection
+                string editValueQuery = "UPDATE dbo.UNITS SET VALUE = @Value WHERE UNIT_NAME = @UnitName";
+                using (var editValue = new SqlCommand(editValueQuery, connection))
+                {
+                    editValue.Parameters.AddWithValue("@Value", value);
+                    editValue.Parameters.AddWithValue("@UnitName", unitName);
+                    _ = editValue.ExecuteNonQuery();
+                }
+            }
         }
-            
     }
 }
